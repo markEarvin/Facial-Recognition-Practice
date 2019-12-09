dc24c626294a528586b36526d8b46e14330e9aa5
markEarvin <mark.earvin@gmail.com>
2019-12-09 16:33:30 +0800

    added canvas where we will draw the face detections

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ const imageUpload = document.getElementById('imageUpload');
      document.body.append("Loaded");
      imageUpload.addEventListener('change', async () => {
         const image = await faceapi.bufferToImage(imageUpload.files[0]);
+        const canvas = faceapi.createCanvasFromMedia(image);
+        imgContainer.append(image);
+        imgContainer.append(canvas);
         const detections = await faceapi.detectAllFaces(image)
         .withFaceLandmarks().withFaceDescriptors();
      });
