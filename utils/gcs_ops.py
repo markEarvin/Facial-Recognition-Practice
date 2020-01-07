bcf9624a8b609dbc33a56f181eafc98ef7009ca4
markEarvin <mark.earvin@gmail.com>
2020-01-07 14:48:42 +0800

    we can now use our script via the command line by providing command interface

--- utils/gcs_ops.py
+++ utils/gcs_ops.py
@@ -126,78 +126,98 @@ def get_files(path="./"):
     ret = []
     return ret
 
-def begin_upload():
-    dirs = get_sub_dirs()
-    for sub_dir in dirs:
-        files = get_files(sub_dir)
-        for file_ in files:
-            path = get_hash(sub_dir) + SEP + file_
-            print path
+def begin_upload(bucket, path="./"):
+    print ("uploading blobs ...")
+    try:
+        folders = scan_for_folders(path)
+        for folder in folders:
+            folder_hash = get_hash(folder)
+            files = scan_for_files_or_objects(folder)
+            for fitem in files:
+                object_name = folder_hash + "/" + fitem.get("filename", "")
+                upload_blob(bucket, fitem.get("filepath", "./"), object_name)
+        print ("done uploading blobs ...")
+    except Exception as e:
+        print (e)
 
-def test_upload_blob():
+def test_upload_blob(bucket = "lala_face_recognition_test"):
     """
         tests our uploading capability by performing the following:
         1. scan current wording directory for sub folders
         2. create a dictionary of all images in the subfolders
-        3. upload each images as blob (SHA256_Digest of parent folder + SEP + filename)
-    """
-    folders = scan_for_folders()
-    bucket = "lala_face_recognition_test"
-    for folder in folders:
-        folder_hash = get_hash(folder)
-        print (folder_hash)
-        files = scan_for_files_or_objects(folder)
-        print (files)
-        for fitem in files:
-            object_name = folder_hash + "/" + fitem.get("filename", "")
-            upload_blob(bucket, fitem.get("filepath", "./"), object_name)
+        3. upload each images as blob (SHA256_Digest of parent folder + '/' + filename)
+    """    
+    print ("this is test. uploading subfolders to " + bucket + "...")
+    try:
+        folders = scan_for_folders()
+        for folder in folders:
+            folder_hash = get_hash(folder)
+            print (folder_hash)
+            files = scan_for_files_or_objects(folder)
+            print (files)
+            for fitem in files:
+                object_name = folder_hash + "/" + fitem.get("filename", "")
+                upload_blob(bucket, fitem.get("filepath", "./"), object_name)
+    except Exception as e:
+        print (e)
 
 
 
 def print_help():
-    print """Usage: python gcs_objects.py <command>
+    print """Usage: python gcs_ops.py <command> <bucket name> ..optional <path>
+    example: python gcs_ops.py upload test_bucket /images/tests
     Command can be:
         help: Prints this help
         list: Lists all the objects in the specified bucket
-        create: Upload the provided file in specified bucket
-        delete: Delete the provided filename from bucket
+        upload: Upload the provided files under all subfolders in specified bucket
+
+    TODO: flags
     """
 
 
 # [END storage_list_files]
 
 
-if __name__ == "__main__":
-    # list_blobs(bucket_name=sys.argv[1])
-    # for testing
-    test_upload_blob()
-
-
 # if __name__ == "__main__":
-#     if len(sys.argv) < 2 or sys.argv[1] == "help" or \
-#         sys.argv[1] not in ['list', 'create', 'delete', 'get']:
-#         print_help()
-#         sys.exit()
-#     if sys.argv[1] == 'list':
-#         if len(sys.argv) == 3:
-#             list_objects(sys.argv[2])
-#             sys.exit()
-#         else:
-#             print_help()
-#             sys.exit()
-#     if sys.argv[1] == 'create':
-#         if len(sys.argv) == 4:
-#             # create_object(sys.argv[2], sys.argv[3])
-#             pass
-#             sys.exit()
-#         else:
-#             print_help()
-#             sys.exit()
-#     if sys.argv[1] == 'delete':
-#         if len(sys.argv) == 4:
-#             # delete_object(sys.argv[2], sys.argv[3])
-#             pass
-#             sys.exit()
-#         else:
-#             # print_help()
-#             sys.exit()
+#     # list_blobs(bucket_name=sys.argv[1])
+#     # for testing
+#     test_upload_blob()
+
+
+if __name__ == "__main__":
+    print (sys.argv)
+    if len(sys.argv) < 2 or sys.argv[1] == "help" or \
+        sys.argv[1] not in ['list', 'upload', 'test']:
+        print_help()
+        sys.exit()
+    if sys.argv[1] == 'list':
+        if len(sys.argv) == 2:
+            list_buckets()
+            sys.exit()
+        elif len(sys.argv) == 3:
+            list_blobs(sys.argv[2])
+            sys.exit()
+        else:
+            print_help()
+            sys.exit()
+    if sys.argv[1] == 'upload':
+        if len(sys.argv) >= 3:
+            if (len(sys.argv) > 3):
+                begin_upload(sys.argv[2], sys.argv[3])
+            else:
+                begin_upload(sys.argv[2])
+            sys.exit()
+        else:
+            print_help()
+            sys.exit()
+
+    if sys.argv[1] == 'test':
+        if len(sys.argv) == 3:
+            test_upload_blob(sys.argv[2])
+            sys.exit()
+        elif len(sys.argv) == 2:
+            test_upload_blob()
+            sys.exit()
+        else:
+            print_help()
+            sys.exit()
