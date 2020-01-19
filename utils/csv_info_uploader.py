ea0d6470e17145c7c274c35f9c0bd7c12162059f
markEarvin <mark.earvin@gmail.com>
2020-01-19 11:44:24 +0800

    added capability to upload to Datastore in GCP. Now we only need to test

--- utils/csv_info_uploader.py
+++ utils/csv_info_uploader.py
@@ -1,12 +1,14 @@
 from google.cloud import datastore
 import google.cloud.exceptions
+
+from random import seed
+from random import randint
+
 import csv
 
 ENTRIES_PER_PAGE = 10
 
-def read_and_upload_csv(path=""):
-    # for testing only, set the path locally
-    path = 'DTC CPS Employee List.csv'
+def read_and_upload_csv(path="", client=None):
     rows = []
     with open(path) as csvfile:
         reader = csv.DictReader(csvfile)
@@ -24,10 +26,15 @@ def read_and_upload_csv(path=""):
         if len(row_entry):
             rows.append(row_entry)
     # now let's upload per page
+    print (len(rows))
     for entry in rows:
-        upload_entries(entry)
+        upload_entries(entry, client)
 
 def transform_row(row):
+    """
+    Set all keys to lower case and replace all spaces with underscore '_'.
+    Creating a new dictionary altogether.
+    """
     transformed = {}
     for key in row.keys():
         new_key = key.lower()
@@ -35,29 +42,31 @@ def transform_row(row):
         transformed[new_key] = row.get(key, "")
     return transformed
 
+def get_random_number(seed_value = 1):
+    seed(seed_value)
+    return randint(0, 9999999)
+
 def upload_entries(rows, client=None):
     print (len(rows))
-    # task1 = datastore.Entity(client.key('Task', 1))
+    # tasks = []
+    # try:
+    #     for entry in rows:
+    #         task = datastore.Entity(client.key('Employee', entry.get("eid", get_random_number())))
+    #         task.update(entry)
+    #         tasks.append(task)
+    #     client.put_multi(tasks)
+    # except Exception as e:
+    #     print (e)
 
-    # task1.update({
-    #     'category': 'Personal',
-    #     'done': False,
-    #     'priority': 4,
-    #     'description': 'Learn Cloud Datastore'
-    # })
+def main(project_id, path):
+    client = datastore.Client(project_id)
+    read_and_upload_csv(path, client)
 
-    # task2 = datastore.Entity(client.key('Task', 2))
 
-    # task2.update({
-    #     'category': 'Work',
-    #     'done': False,
-    #     'priority': 8,
-    #     'description': 'Integrate Cloud Datastore'
-    # })
-
-    # client.put_multi([task1, task2])
-
+# how to authenticate: follow the providing service account credentials section here:: https://cloud.google.com/docs/authentication/production
 
 if __name__ == "__main__":
     # print (sys.argv)
-    read_and_upload_csv()
+    project_id = "august-clover-261601"
+    path = 'DTC CPS Employee List.csv'
+    main(project_id, path)
